d33c4b041eea74dd3e54446be2dd835a462a1616
danielFrancisOlivieri <dfolivieri@gmail.com>
2020-09-27 13:14:45 -0400

    reworked layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,30 +6,8 @@ import { rhythm, scale } from "../utils/typography"
 
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
+ // const rootPath = `${__PATH_PREFIX__}/`
   let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
     header = (
       <h3
         style={{
@@ -48,7 +26,7 @@ const Layout = ({ location, title, children }) => {
         </Link>
       </h3>
     )
-  }
+  
   return (
     <div
       style={{
