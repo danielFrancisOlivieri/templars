8ad3a434412b9f6dd5ef58f02ea4a347178e3060
danielFrancisOlivieri <dfolivieri@gmail.com>
2020-09-27 12:25:01 -0400

    Revert "removed query from hunt page"
    This reverts commit 516c6c4c2a79b75f24a577ba7d5196ff7a60782c.

--- src/pages/hunt.js
+++ src/pages/hunt.js
@@ -1,14 +1,57 @@
 import React from "react"
-import Layout from "../components/layout"
-import './style/poetry.css'
-import 'semantic-ui-css/semantic.min.css'
+import { Link, graphql } from "gatsby"
 
-const hunt = ({ data, location }) => {
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+import Image from "gatsby-image"
+
+
+const Hunt = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+  console.log(location);
+
   return (
-    <Layout location={location} title={'Literature+Code'}>
-   <h3> scavenger hunt</h3>
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+
+     <p>
+     It is the <span id="casablanca" onclick="document.getElementById('casablanca').innerHTML = 'correct'" >phenomenon</span> sometimes called alienation from self. In its advanced stages, we no longer answer the telephone, because someone might want something; that we could say no without drowning in self-reproach is an idea alien to this game. 
+     Every encounter demands too much, tears the nerves, drains the will, and the spectre of something as small as an unanswered letter arouses such disproportionate guilt that one's sanity becomes an object of speculation among one's acquaintances. 
+     To assign unanswered letters their proper weight, to free us from the expectations of others, to give us back to ourselves—there lies the great, the singular power of self-respect. 
+     Without it, one eventually discovers the final turn of the screw: one runs away to find oneself, and finds no one at home.
+     </p>
+      
+     
     </Layout>
   )
 }
 
-export default hunt
+export default Hunt
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
