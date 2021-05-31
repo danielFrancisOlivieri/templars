9b16775bb980fdfdbf4e9ccb1342dd147f2a7ba2
danielFrancisOlivieri <dfolivieri@gmail.com>
2021-05-31 13:57:01 -0400

    Added smaller versions
    Now I have a template that mostly just needs text.

--- src/pages/pragma.js
+++ src/pages/pragma.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, ReferenceLine, Tooltip, Legend } from 'recharts';
 import { PragmaToolTip } from '../components/pragma/tooltip/tooltip';
+import { Button, Input, Modal } from 'semantic-ui-react'
 import Layout from "../components/layout"
 
 const strangers = [
@@ -29,14 +30,18 @@ export default class Example extends PureComponent {
         super(props);
         this.state = {
           newPoints: [],
-          activeDotName: ''
+          activeDotName: '',
+          modalOpen: false
         };
       }
 
       handleClick = (o) => {
-          console.log(o);
-          this.setState({ newPoints: [...this.state.newPoints, {name: 'Original', x: o.xValue, y: o.yValue }] })
+          console.log(o);          
+          this.setState({ newPoints: [...this.state.newPoints, {name: 'New', x: o.xValue, y: o.yValue }] })
       }
+    //   handleDotClick = (o) => {
+    //     this.setState({modalOpen: true});
+    //   }
       handleMouseEnter = (o) => {
         console.log(o);
         this.setState({activeDotName: o.name});
@@ -72,10 +77,109 @@ export default class Example extends PureComponent {
         <Scatter name="New" data={this.state.newPoints} fill="#F26419" onMouseEnter={this.handleMouseEnter} />
       </ScatterChart>
       </center>
-      <h2>Does Familiarity Breed contempt?</h2>
+      <h2>Lovers Only</h2>
       <p>
       I want to bring different ways of understanding into the same fold, to have them work together to in their different ways, with their different strengths to assemble an experience and an understanding that unwraps what had been hidden. II want Montaigne next to a graph next to a statistic next to the findings of a study next to Emerson next to my own opinions next to a bibliography. I want a map too. I want to press the full powers of the website into service of a single idea, or a dozen ideas indexed together by a single concept, to see the unifying principle behind disparate realities. The idea behind the thing in the world. 
       </p>
+
+      <center>
+  <ScatterChart
+        width={340}
+        height={340}
+        margin={{
+          top: 5,
+          right: 20,
+          left: 20,
+          bottom: 5
+        }}
+        onClick={this.handleClick}
+      >
+        <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="affection" domain={[0, 100]} />
+            <YAxis type="number" dataKey="y" name="knowledge" domain={[0, 100]} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} content={<PragmaToolTip title={this.state.activeDotName} />} />
+            <Legend />
+            <ReferenceLine y={50} stroke="red" />
+            <ReferenceLine x={50} stroke="red" />
+        <Scatter name="Lovers" data={lovers} fill="#D33E43" />
+      </ScatterChart>
+      </center>
+
+      <h2>Enemies</h2>
+      <p>
+      I want to bring different ways of understanding into the same fold, to have them work together to in their different ways, with their different strengths to assemble an experience and an understanding that unwraps what had been hidden. II want Montaigne next to a graph next to a statistic next to the findings of a study next to Emerson next to my own opinions next to a bibliography. I want a map too. I want to press the full powers of the website into service of a single idea, or a dozen ideas indexed together by a single concept, to see the unifying principle behind disparate realities. The idea behind the thing in the world. 
+      </p>
+
+      <center>
+  <ScatterChart
+        width={340}
+        height={340}
+        margin={{
+          top: 5,
+          right: 20,
+          left: 20,
+          bottom: 5
+        }}
+        onClick={this.handleClick}
+      >
+        <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="affection" domain={[0, 100]} />
+            <YAxis type="number" dataKey="y" name="knowledge" domain={[0, 100]} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} content={<PragmaToolTip title={this.state.activeDotName} />} />
+            <Legend />
+            <ReferenceLine y={50} stroke="red" />
+            <ReferenceLine x={50} stroke="red" />
+        <Scatter name="Enemies" data={enemies} fill="#2191FB" />
+      </ScatterChart>
+      </center>
+
+      <h2>Strangers</h2>
+      <p>
+      I want to bring different ways of understanding into the same fold, to have them work together to in their different ways, with their different strengths to assemble an experience and an understanding that unwraps what had been hidden. II want Montaigne next to a graph next to a statistic next to the findings of a study next to Emerson next to my own opinions next to a bibliography. I want a map too. I want to press the full powers of the website into service of a single idea, or a dozen ideas indexed together by a single concept, to see the unifying principle behind disparate realities. The idea behind the thing in the world. 
+      </p>
+
+      <center>
+  <ScatterChart
+        width={340}
+        height={340}
+        margin={{
+          top: 5,
+          right: 20,
+          left: 20,
+          bottom: 5
+        }}
+        onClick={this.handleClick}
+      >
+        <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="affection" domain={[0, 100]} />
+            <YAxis type="number" dataKey="y" name="knowledge" domain={[0, 100]} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} content={<PragmaToolTip title={this.state.activeDotName} />} />
+            <Legend />
+            <ReferenceLine y={50} stroke="red" />
+            <ReferenceLine x={50} stroke="red" />
+        <Scatter name="Enemies" data={strangers} fill="#CCDBDC" />
+      </ScatterChart>
+      </center>
+
+
+        <h2> Bibliography</h2>
+        <p> Lorem ipsum </p>
+
+      <Modal
+        size='small'
+        open={this.state.modalOpen}
+      >
+        <Modal.Header>Edit Point Name</Modal.Header>
+        <Modal.Content>
+        <Input placeholder='Point name' />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button positive onClick={()=> this.setState({modalOpen: false}) } >
+            Done
+          </Button>
+
+        </Modal.Actions>
+      </Modal>
     
       </Layout>
       
